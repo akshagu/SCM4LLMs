8d99d3ebb82e0971fed000333d2e6a1db698cec4
akshaygu <akshay180294@gmail.com>
2023-08-02 02:30:42 -0400

    feat: add summarization prompts

--- prompts/book.py
+++ prompts/book.py
@@ -116,3 +116,21 @@ Summaries of each paragraph:
 
 Document Summary:
 """
+
+en_turn_summarization_prompt = """Below is a conversation between a user and an AI assistant. Please write a summary for each of them in one sentence and list them in separate paragraphs, while trying to preserve the key information of the user’s question and the assistant’s answer as much as possible.
+
+conversation content: 
+
+{input}
+
+Summary:
+"""
+
+zh_turn_summarization_prompt = """以下是用户和人工智能助手的一段对话，请分别用一句话写出用户摘要、助手摘要，分段列出，要求尽可能保留用户问题和助手回答的关键信息。
+
+对话内容： 
+
+{input}
+
+摘要：
+"""
